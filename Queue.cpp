8e025b4d3d527f39e1cf1db9a154721022b1c603
Pragyan Khati <pragyankhati45@gmail.com>
2025-07-23 13:17:33 +0545

    Manual Queue

---
+++ Queue.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+    private:
+        int arr[5];
+        int back{-1};
+        int front{-1};
+    public:
+        Queue(){  for(int i = 0 ; i < 5 ; i ++){arr[i] = 0;}  }
+        bool isEmpty(){return (front==-1 && back==-1)? true:false;}
+        bool isFull(){return (back==size(arr)-1)? true:false;}
+
+       void enqueue(int x){
+            if (isFull()){cout<<"Cam not add"<<endl; return;}
+            else if(isEmpty()){front+=1;back+=1;}
+            else{back+=1;}
+            arr[back]=x;
+        }
+       int dequeue(){
+            int x{};
+            if (isEmpty()){cout<<"Cannot not remove"<<endl; return -1;}
+            else if(front==back){x = arr[front]; arr[front] =0; front -=1; back -=1;}
+            else{x = arr[front]; arr[front] = 0 ; front+=1;}
+            return x;
+       }
+       int count(){
+            return (isEmpty())? 0:(back-front+1);
+       }
+       void display(){
+            cout<<"Your elements in the queue are: "<<endl;
+            for(int i{} ; i < 5 ; i ++ ){
+                cout<<arr[i]<<" ";
+            }
+            cout<<""<<endl;
+       }
+};
+
+int main() {
+    Queue q1;
+    int option{};
+    int value{};
+
+    cout<<boolalpha;
+    cout << "Your options:" << endl;
+    cout << "(1) Is the queue empty?\n(2) Is the queue full?\n(3) Enqueue\n(4) Dequeue\n(5) Count elements\n(6) Display queue\n(-1) Exit\n";
+    while (option != -1) {
+        cout << "Enter your option: ";
+        cin >> option;
+
+        switch(option) {
+            case 1:
+                cout << "Queue is empty: " << q1.isEmpty() << endl;
+                break;
+            case 2:
+                cout << "Queue is full: " << q1.isFull() << endl;
+                break;
+            case 3:
+                cout << "Enter value to enqueue: ";
+                cin >> value;
+                q1.enqueue(value);
+                break;
+            case 4:
+                cout << "Dequeued value: " << q1.dequeue() << endl;
+                break;
+            case 5:
+                cout << "Number of elements: " << q1.count() << endl;
+                break;
+            case 6:
+                q1.display();
+                break;
+            case -1:
+                cout << "Program exited." << endl;
+                break;
+            default:
+                cout << "Invalid option." << endl;
+                break;
+        }
+    }
+    return 0;
+}
